48824669ce1f00512d7c4672ce82e44c3dc7a650
Abhishek Bhardwaj <ayushindia4@gmail.com>
2025-12-12 20:13:49 +0530

    Setup frontend code and native wind in mobile frontend

---
+++ mobile/app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import '../global.css'
+
+
+export default function Index() {
+  return (
+    <View
+      className="flex flex-col items-center justify-center bg-black h-full"
+    >
+      <Text className="text-white text-xl font-bold">Hello bro</Text>
+    </View>
+  );
+}
